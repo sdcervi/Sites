f332c4f801d5011aeb452b08573a61f3654373d5
sdcervi <sdcervi@gmail.com>
2020-05-07 15:38:25 -0700

    Update sample.php
    Removed unneeded text in jumbotron explainer

--- sample.php
+++ sample.php
@@ -31,7 +31,7 @@
 
 	<section class="jumbotron text-center">
 		<div class="container">
-			<p class="lead text-muted">This is a sample dex (mine, actually) to show what the caught/not caught difference would be.  Caught Pok&eacute;mon will be full color with names under them, and Pok&eacute;mon that haven't yet been caught will be very faint.</p>				
+			<p class="lead text-muted">This is a sample dex to show what the caught/not caught difference would be.  Caught Pok&eacute;mon will be full color with names under them, and Pok&eacute;mon that haven't yet been caught will be very faint.</p>				
 		</div>
 	</section>
 	
