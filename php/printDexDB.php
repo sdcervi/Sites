5b4c616997ee80b2bbf540f593c2bb18b916bf6d
sdcervi <sdcervi@gmail.com>
2020-05-29 20:03:16 -0700

    Debug mysql
    add require to see if it can access the file

--- php/printDexDB.php
+++ php/printDexDB.php
@@ -1,5 +1,6 @@
 <?php
 
+require '/var/www/html/config.ini';
 $config = parse_ini_file('/var/www/html/config.ini');
 $connection = new mysqli ($config['host'], $config['username'], $config['password'], $config['db']);
 // We should definitely set up a different user so that root isn't used for this
